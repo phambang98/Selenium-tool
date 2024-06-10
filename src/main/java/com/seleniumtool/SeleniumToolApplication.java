424ff8fa0b399c6b9777698c48eb41cac15d3848
BANGPT-PC\bangpt <bangpt>
2024-06-10 15:19:54 +0700

    first commit

---
+++ src/main/java/com/seleniumtool/SeleniumToolApplication.java
@@ -0,0 +1,13 @@
+package com.seleniumtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumToolApplication.class, args);
+	}
+
+}
